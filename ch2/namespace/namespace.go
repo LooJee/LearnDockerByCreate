e0c37642cb19589f163072fbb97b4fea2bc8df1f
zer0 <mynameislujie1992@gmail.com>
2020-10-28 19:49:13 +0800

    mount namespace

--- ch2/namespace/namespace.go
+++ ch2/namespace/namespace.go
@@ -10,9 +10,11 @@ import (
 func main() {
 	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWIPC |
+			syscall.CLONE_NEWNS,
 	}
-
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
