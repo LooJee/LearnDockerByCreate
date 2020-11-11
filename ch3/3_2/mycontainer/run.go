8cd8db9bb025a1c55798329d9bb2e1920937cae1
zer0 <mynameislujie1992@gmail.com>
2020-11-11 15:17:51 +0800

    限制容器资源

---
+++ ch3/3_2/mycontainer/run.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"LearnDockerByCreate/ch3/3_2/mycontainer/cgroups"
+	"LearnDockerByCreate/ch3/3_2/mycontainer/cgroups/subsystems"
+	"LearnDockerByCreate/ch3/3_2/mycontainer/container"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"strings"
+)
+
+func Run(tty bool, comArray []string, res *subsystems.ResourceConfig) {
+	parent, writePipe := container.NewParentProcess(tty)
+	if parent == nil {
+		log.Errorf("New parent process error")
+		return
+	}
+	if err := parent.Start(); err != nil {
+		log.Error(err)
+	}
+	// use mydocker-cgroup as cgroup name
+	cgroupManager := cgroups.NewCgroupManager("mydocker-cgroup")
+	defer cgroupManager.Destroy()
+	cgroupManager.Set(res)
+	cgroupManager.Apply(parent.Process.Pid)
+
+	sendInitCommand(comArray, writePipe)
+	parent.Wait()
+}
+
+func sendInitCommand(comArray []string, writePipe *os.File) {
+	command := strings.Join(comArray, " ")
+	log.Infof("command all is %s", command)
+	writePipe.WriteString(command)
+	writePipe.Close()
+}
